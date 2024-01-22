91f6815840dcb6ebe20caa6bba141e4370186a3b
Viviane Locatelli <88899838+vivianeLocatelli@users.noreply.github.com>
2024-01-22 14:42:26 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import databade from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await databade.query("SELECT 1 + 1 as SUM;");
+  console.log(result.rows);
   response.status(200).json({ status: "200" });
 }
 
