b3630f36e915cd289307ea1b04fb4937ba4766f2
Viviane Locatelli <88899838+vivianeLocatelli@users.noreply.github.com>
2024-08-12 13:53:32 +0000

    finish ` /api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
-import databade from "infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await databade.query("SELECT 1 + 1 as SUM;");
-  console.log(result.rows);
-  response.status(200).json({ status: "200" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseInfo = await database.query(`SELECT
+    current_setting('server_version') AS server_version,
+    current_setting('max_connections') AS max_connections`);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConnections = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    database_info: {
+      server_version: databaseInfo.rows[0].server_version,
+      max_connections: Number(databaseInfo.rows[0].max_connections),
+      current_connections: Number(activeConnections.rows[0].count),
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,22 @@
-test("GET to /api/v1/status should retun 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+beforeAll(async () => {
+  response = await fetch("http://localhost:3000/api/v1/status");
+  responseJson = await response.json();
+});
+
+describe("GET to /api/v1/status", () => {
+  test("should retun 200", () => {
+    expect(response.status).toBe(200);
+  });
+  test("should contain updated date", () => {
+    expect(responseJson.updated_at).toBeDefined();
+
+    const parseUpdatedAt = new Date(responseJson.updated_at).toISOString();
+    expect(responseJson.updated_at).toEqual(parseUpdatedAt);
+  });
+  test("database should return valid values", () => {
+    expect(responseJson.database_info).toBeDefined();
+    expect(responseJson.database_info.server_version).toMatch(/16.0/);
+    expect(responseJson.database_info.max_connections).toBe(100);
+    expect(responseJson.database_info.current_connections).toEqual(1);
+  });
 });
